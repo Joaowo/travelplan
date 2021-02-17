cf15af7e42756a793c35bf240d4ed89332f82897
João Amado <joaowebpoa@gmail.com>
2021-02-17 00:34:22 -0300

    Https test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://3.137.211.94:5000/v1/',
+  baseURL: 'https://3.137.211.94:5000/v1/',
   headers: {
     Accept: '*/*',
     'Content-Type': 'application/json'
